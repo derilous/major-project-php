74fba863b6fda8be6cbb4e2f3bfc536fc96bbf4b
Iskandar <nikizkandar@gmail.com>
2021-06-11 05:58:03 +0800

    Annoucement Settings UI
    cleaned ui

--- announcementSettings.php
+++ announcementSettings.php
@@ -5,51 +5,56 @@
 <head>
 	<meta charset="utf-8">
     <title>Admin Controls</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/anouncementSettings.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/announcementSettings.css">
 </head>
 <body>
 
  <div id="background">
-        <div id="anouncementTitle">
+        <div id="announcementTitle">
             <div id="addContainer">
             	<div id="inputContainer">
-                    <div>Announcement Settings</div>
+                    <h2>Announcement Settings</h2>
             		<form id="addAnnouncement" action="insertData.php" method="POST" enctype="multipart/form-data">
-                        <div>Add a new announcement:</div>
-                        <select name="type">
-                        <option value="1">Promotion</option>
-                        <option value="2">Special Discount</option>
-                        <option value="3">Event</option>
-                        </select>
+                        <label>Post Announcement</label>
+                            <select name="type">
+                                <option value="1">Promotion</option>
+                                <option value="2">Special Discount</option>
+                                <option value="3">Event</option>
+                                    </select>
                         <div>
-                        <input id="Title" name="Title" placeholder="Title" type="text" required>
+                            <input id="Title" name="Title" placeholder="Title" type="text" required>
                         </div>
                         <div>
-                        <input id= "Period" name="Period" placeholder="Period" type="text" required>
+                            <input id= "Period" name="Period" placeholder="Date" type="text" required>
                         </div>
                         <div>
-                        <input id= "Description" name="Description" placeholder="Description" required>
+                            <input id= "Description" name="Description" placeholder="Description" type="text" required>
                         </div>
                         <div>
-                        <input id = "image" name="image" placeholder = "Add image web link" required>
+                            <input id = "image" name="image" placeholder = "Image Link" type="text" required>
                         </div>
                         <div>
-                        <button type="submit" name = "submitButton">Add Announcement</button>
+                             <button type="submit" name = "submitButton">ADD</button>
                         </div>
                     </form>
+                    <p>
+                <div id="deleteContainer">
                     <form id ="deleteAnnouncement" action = "deleteAnnouncement.php" method="POST">
-                        <div>Delete an announcement</div>
+                        <label>Remove Announcement</label>
                         <div>
-                        <select name="typeDelete">
-                            <option value="1">Promotion</option>
-                            <option value="2">Special Discount</option>
-                            <option value="3">Event</option>
+                            <select name="typeDelete">
+                                <option value="1">Promotion</option>
+                                <option value="2">Special Discount</option>
+                                <option value="3">Event</option>
                             </select>
-                            <input id ="id" name = "id" placeholder = "Insert announcement ID" required>
-                            <button type="submit" name = "submitDelete">Delete Announcement</button>
-                        <div>
-                    </div>
-                 </div>
+                            <br>
+                            <input id ="id" name = "id" placeholder = "Announcement ID" type="text" required>
+                            <button type="submit" name = "submitDelete">REMOVE</button>
+                        <div/>
+                </div>
+                </div>
+        </div>
+</div>
         </form>
 </body>
 </html>
